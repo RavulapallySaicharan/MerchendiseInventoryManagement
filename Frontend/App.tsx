ee54bfb40c97975174ef9155a2131bceb0a8dbce
saicharan <ravulapally.saicharan@gmail.com>
2025-02-18 23:49:56 +0530

    front end working base

--- Frontend/App.tsx
+++ Frontend/App.tsx
@@ -5,11 +5,11 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-d
 import LandingPage from "./components/LandingPage"
 import LoginPage from "./components/LoginPage"
 import HomePage from "./components/HomePage"
-import InventoryPage from "./components/InventoryPage"
-import AlertsPage from "./components/AlertsPage"
-import BatchesPage from "./components/BatchesPage"
-import SuppliersPage from "./components/SuppliersPage"
-import AnalyticsPage from "./components/AnalyticsPage"
+// import InventoryPage from "./components/InventoryPage"
+// import AlertsPage from "./components/AlertsPage"
+// import BatchesPage from "./components/BatchesPage"
+// import SuppliersPage from "./components/SuppliersPage"
+// import AnalyticsPage from "./components/AnalyticsPage"
 
 const App: React.FC = () => {
   return (
@@ -18,11 +18,11 @@ const App: React.FC = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
-        <Route path="/inventory" element={<InventoryPage />} />
+        {/* <Route path="/inventory" element={<InventoryPage />} />
         <Route path="/alerts" element={<AlertsPage />} />
         <Route path="/batches" element={<BatchesPage />} />
         <Route path="/suppliers" element={<SuppliersPage />} />
-        <Route path="/analytics" element={<AnalyticsPage />} />
+        <Route path="/analytics" element={<AnalyticsPage />} /> */}
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </Router>
